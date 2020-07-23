a38ec07d064c25e79416c4d11da8e233ca05d474
Makena-Maryann <maryann.makena00@gmail.com>
2020-07-23 14:38:41 +0300

    Add app configurations

---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://maryann:Maryann00*@localhost/blog'
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class TestConfig(Config):
+    '''
+    Test configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+config_options = {
+ 'development':DevConfig,
+ 'production':ProdConfig,
+ 'test':TestConfig
+}           
